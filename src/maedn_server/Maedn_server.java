70fbfc9b4644c1a85d88487bd63a031d496c5902
Heiko K <heiko.kuhfuss@mni.thm.de>
2014-06-20 12:02:23 +0200

    inital commit

---
+++ src/maedn_server/Maedn_server.java
@@ -0,0 +1,12 @@
+package maedn_server;
+
+public class Maedn_server {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+    
+}
